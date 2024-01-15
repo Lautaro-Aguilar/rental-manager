65b87e5d9136c5cb12dfa73e02984323bdc6014b
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2024-01-15 20:00:14 -0300

    create data table

--- app/dashboard/[houseId]/(home)/page.tsx
+++ app/dashboard/[houseId]/(home)/page.tsx
@@ -1,6 +1,9 @@
 import Example from "../_components/areachart";
+import { columns } from "../_components/columns";
+import { DataTable } from "../_components/data-table";
 import { Resumes } from "../_components/resumes";
 import { WelcomeBackCard } from "../_components/welcome-back";
+import { format } from "date-fns";
 
 interface DashboardPageProps {
   params: {
@@ -8,20 +11,70 @@ interface DashboardPageProps {
   };
 }
 
+const blockedUsers = [
+  {
+    id: "1",
+    blocked: {
+      id: "1",
+      imageUrl:
+        "https://images.unsplash.com/photo-1556742042-ec1d3b7c3e0e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFtaWx5JTIwYm9va3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      username: "Lauta",
+      createdAt: "2021-07-06T21:24:19.000Z",
+    },
+  },
+  {
+    id: "2",
+    blocked: {
+      id: "2",
+      imageUrl:
+        "https://images.unsplash.com/photo-1556742042-ec1d3b7c3e0e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFtaWx5JTIwYm9va3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      username: "John Doe",
+      createdAt: "2021-07-06T21:24:19.000Z",
+    },
+  },
+  {
+    id: "3",
+    blocked: {
+      id: "3",
+      imageUrl:
+        "https://images.unsplash.com/photo-1556742042-ec1d3b7c3e0e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFtaWx5JTIwYm9va3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      username: "John Doe",
+      createdAt: "2021-07-06T21:24:19.000Z",
+    },
+  },
+  {
+    id: "4",
+    blocked: {
+      id: "3",
+      imageUrl:
+        "https://images.unsplash.com/photo-1556742042-ec1d3b7c3e0e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFtaWx5JTIwYm9va3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      username: "John Doe",
+      createdAt: "2021-07-06T21:24:19.000Z",
+    },
+  },
+];
+
 const DashboardPage = ({ params }: DashboardPageProps) => {
-  console.log(params.houseId);
+  const formattedData = blockedUsers.map((block) => ({
+    ...block,
+    userId: block.blocked.id,
+    imageUrl: block.blocked.imageUrl,
+    username: block.blocked.username,
+    createdAt: format(new Date(block.blocked.createdAt), "dd/MM/yyyy"),
+  }));
+
   return (
     <div className="h-full p-8 max-w-screen-2xl mx-auto">
       <Resumes />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 py-6 px-3 max-h-72">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 py-6 ml-2">
         <div className="col-span-2 h-full">
           <WelcomeBackCard />
         </div>
         <div className="col-span-2">
           <Example />
         </div>
-        {/* Add your other cards here */}
       </div>
+      <DataTable columns={columns} data={formattedData} />
     </div>
   );
 };
---
+++ app/dashboard/[houseId]/_components/columns.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { ArrowUpDown, UserCheck } from "lucide-react";
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+
+export type BlockedUser = {
+  id: string;
+  userId: string;
+  imageUrl: string;
+  username: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<BlockedUser>[] = [
+  {
+    accessorKey: "username",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Username
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-4">
+        <UserCheck className="w-4 h-4" />
+        <span>{row.original.username}</span>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Date blocked
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <Button onClick={() => console.log(row)}>Test</Button>,
+  },
+];
